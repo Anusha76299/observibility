6de473379902ec4c43f25d0a0f8c251894953acd
Anusha76299 <111690644+Anusha76299@users.noreply.github.com>
2024-06-03 16:50:40 +0530

    Update Data_Base_Connection.py

--- Data_Base_Connection.py
+++ Data_Base_Connection.py
@@ -5,7 +5,7 @@ from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT
 
 def get_connection():
     conn = psycopg2.connect(
-        dbname=DB_NAME,
+        dbname=postgres,
         user=DB_USER,
         password=DB_PASSWORD,
         host=postgres,
